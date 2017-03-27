9ea7ddfbe08e9f4d1ad5710da70b7fbbb2525d09
Assil Ksiksi <cyph0nik@gmail.com>
2017-03-26 22:01:12 -0400

    Initial implementation of predictor

--- include/predictor.hpp
+++ include/predictor.hpp
@@ -1,9 +1,24 @@
 #ifndef PREDICTOR_HPP
 #define PREDICTOR_HPP
 
+#include <vector>
 
+/*
+ * Implementation of n-entry k-bit Smith counter GSelect with GHR.
+ * All Smith counter initialized to 01. GHR = 000 by default.
+ */
 class BranchPredictor {
+public:
+    BranchPredictor(int n);
+    bool predict(int address);
+    void update(int address, bool taken);
+private:
+    std::vector<std::vector<int>> prediction_table;
+    int n; // Number of Smith counter entries
+    int k; // Smith counter size, in bits
 
+    int ghr;
+    int ghr_size; // Size of GHR, in bits
 };
 
 #endif
--- src/predictor.cpp
+++ src/predictor.cpp
@@ -1,1 +1,59 @@
+#include <vector>
+
 #include "predictor.hpp"
+
+BranchPredictor::BranchPredictor(int n, int k) : n(n), k(k) {
+    // GHR setup
+    int ghr_size = 3;
+    int ghr = 0;
+
+    // Initialize prediction table (entry = 01 by default)
+    for (int i = 0; i < n; i++) {
+        std::vector<int> counters;
+        for (int j = 0; j < (1 << ghr_size); j++)
+            counter.push_back(1);
+
+        prediction_table.push_back(counters);
+    }
+}
+
+bool BranchPredictor::predict(int address) {
+    // Compute hash of address using given formula
+    int hash = (address / 4) % n;
+
+    // Perform lookup in table to find required Smith counter
+    std::vector<int>& counters = prediction_table[hash];
+    int counter = counters[ghr];
+
+    if (counter >= (1 << k)/2)
+        return true;
+    else
+        return false;
+}
+
+void BranchPredictor::update(int address, bool taken) {
+    // Update Smith counter value based on branch result
+    int hash = (address / 4) % n;
+    std::vector<int>& counters = prediction_table[hash];
+
+    if (taken) {
+        if (counters[ghr] < (1 << k) - 1)
+            counters[ghr] = counters[ghr] + 1;
+    }
+
+    else {
+        if (counters[ghr] > 0)
+            counters[ghr] = counters[ghr] - 1;
+    }
+
+    // Update GHR and table based on branch result
+    int max_size = 1 << ghr_size;
+
+    if (taken) {
+        // Right shift 1 into GHR
+        ghr = ((ghr << 1) + 1) & ((1 << max_size) - 1);
+    } else {
+        // Right shift 0 into GHR
+        ghr = (ghr << 1) & ((1 << max_size) - 1);
+    }
+}
